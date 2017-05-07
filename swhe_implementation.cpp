2e831377a44203f6442e0edf0199119143a421a6
Michael Belland <belland.michael@gmail.com>
2017-05-07 11:08:31 -0700

    base commit for 2017 6.857 SwHE final project

---
+++ swhe_implementation.cpp
@@ -0,0 +1,15 @@
+/* Test_General.cpp - A general test program that uses a mix of 
+operations over four ciphertexts.
+ */
+
+#include <NTL/ZZ.h> 
+#include <NTL/BasicThreadPool.h> 
+#include "FHE.h" 
+#include "timing.h" 
+#include "EncryptedArray.h" 
+#include <NTL/lzz_pXFactoring.h> 
+
+#include <cassert>
+#include <cstdio>
+
+//code goes here
